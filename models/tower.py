a8e887fd66609468750a3b9f0be6ba0024ff1a64
Lunya321 <ivishnevskiy86@gmail.com>
2026-04-20 18:40:39 +0500

    стоимость защитника

--- models/tower.py
+++ models/tower.py
@@ -6,26 +6,28 @@ class Tower:
         self.grid_x = grid_x
         self.grid_y = grid_y
         self.tile_size = tile_size
-
+        
         self.pos = pygame.Vector2(
             grid_x * tile_size + tile_size // 2,
-            grid_y * tile_size + tile_size // 2 
+            grid_y * tile_size + tile_size // 2
         )
-
+        
         self.range = 120
         self.damage = 50
         self.cooldown = 1.0
         self.current_cooldown = 0.0
+        self.cost = 50
 
     def update(self, dt, enemies):
         if self.current_cooldown > 0:
             self.current_cooldown -= dt
-            return 
-        
+            return None
+
         target = self._find_target(enemies)
         if target is not None:
             self.current_cooldown = self.cooldown
             return Projectile(self.pos, target, self.damage)
+            
         return None
 
     def _find_target(self, enemies):
@@ -36,5 +38,5 @@ class Tower:
             distance = self.pos.distance_to(enemy.pos)
             if distance <= self.range:
                 return enemy
-            
+        
         return None
